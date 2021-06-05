f4b4b8b9f077c3bc248b3e616c66c97e808b5469
Nikhil C <nikhil.c@tavant.com>
2021-06-05 17:10:55 +0530

    final

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,6 +13,9 @@ export class DashboardComponent implements OnInit {
   user:User=new User();
   currentUser: any;
   currentScore:any;
+  date:Date;
+  dateintime:number;
+  diff:number;
 
   constructor(private topscoreService:TopscoreService,private token: TokenStorageService) { }
 
@@ -33,9 +36,9 @@ export class DashboardComponent implements OnInit {
     if(this.currentScore<=80)
     this.currentScore=this.getScore(1,20);
     else if(this.currentScore<=95)
-    this.currentScore=this.getScore(21,80);
+    this.currentScore=this.getScore(20,100);
     else 
-    this.currentScore=this.getScore(81,100);
+    this.currentScore=this.getScore(100,180);
     this.user.score=this.user.score+this.currentScore;
     Swal.fire(
       'You got '+this.currentScore+' points',
@@ -59,12 +62,14 @@ export class DashboardComponent implements OnInit {
     if(this.user.ticket>=5){
       this.user.ticket=this.user.ticket-5;
       this.currentScore=this.getScore(1,100);
-      if(this.currentScore<=50)
+      if(this.currentScore<=20)
       this.currentScore=this.getScore(1,20);
-      else if(this.currentScore<=80)
-      this.currentScore=this.getScore(21,80);
-      else 
-      this.currentScore=this.getScore(81,100);
+      else if(this.currentScore<=60)
+      this.currentScore=this.getScore(20,100);
+      else if(this.currentScore<=90)
+      this.currentScore=this.getScore(100,180);
+      else
+      this.currentScore=this.getScore(180,200);
       this.user.score=this.user.score+this.currentScore;
       Swal.fire(
         'You got '+this.currentScore+' points',
@@ -89,12 +94,12 @@ export class DashboardComponent implements OnInit {
     if(this.user.ticket>=10){
       this.user.ticket=this.user.ticket-10;
       this.currentScore=this.getScore(1,100);
-      if(this.currentScore<=20)
-      this.currentScore=this.getScore(1,20);
-      else if(this.currentScore<=50)
-      this.currentScore=this.getScore(21,80);
-      else 
-      this.currentScore=this.getScore(81,100);
+      if(this.currentScore<=10)
+      this.currentScore=this.getScore(21,100);
+      else if(this.currentScore<=30)
+      this.currentScore=this.getScore(101,180);
+      else
+      this.currentScore=this.getScore(180,200);
       this.user.score=this.user.score+this.currentScore;
       Swal.fire(
         'You got '+this.currentScore+' points',
@@ -117,5 +122,31 @@ export class DashboardComponent implements OnInit {
 
   getScore(min:number,max:number){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  getTicket(){
+    // this.topscoreService.getUserById(this.currentUser.id).subscribe(data =>{
+    //   this.user=data;
+    // });
+    this.date=new Date();
+    this.dateintime=this.date.getTime();
+    if(this.user.time<this.dateintime){
+      this.user.time=this.dateintime+3600000;
+      this.user.ticket=this.user.ticket+20;
+      this.topscoreService.updateScoreById(this.user).subscribe(data =>{
+      this.user=data;});
+      Swal.fire(
+        'You got 20 Tickets',
+        'Now your Tickets is '+this.user.ticket,
+        'success'
+      )
+    } else{
+      this.diff=Math.round((this.user.time-this.dateintime)/1000);
+      Swal.fire(
+        'Sorry!',
+        'Come back after '+ this.diff+' seconds',
+        'error'
+      )
+    }
   }
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -5,4 +5,5 @@ export class User {
     // password: string;
     ticket: number;
     score: number;
+    time:number;
 }
